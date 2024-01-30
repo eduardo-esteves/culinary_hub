2b682c322a4b78684625ec3664a51304ed4d6cc7
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2024-01-30 20:07:46 -0300

    Imported the make_recipe function to generate fake recipes for the pages view

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
 
 def home(request):
     context = {
         'title': 'Home',
+        'recipes': [make_recipe() for _ in range(6)],
     }
     return render(request, 'pages/home.html', context)
 
@@ -12,5 +14,6 @@ def home(request):
 def recipe(request, id):
     context = {
         'title': 'Recipe',
+        'recipe': make_recipe(),
     }
     return render(request, 'pages/recipe.html', context)
