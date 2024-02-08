7e847a7c47b1d290a36c751742f9ffe92a3da4fa
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2024-02-07 23:40:09 -0300

    Renamed database name

--- django_core/settings.py
+++ django_core/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'django_core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'culinary_hub.sqlite3',
     }
 }
 
