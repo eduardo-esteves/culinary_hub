de63094ee006678c634e20a7e5425eba337c983a
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2024-02-14 20:32:57 -0300

    Registered category model in django admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from recipes.models import Category, Recipe
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
