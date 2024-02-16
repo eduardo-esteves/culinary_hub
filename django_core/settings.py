91af9e616d470b893a742aa6329c7614e293da74
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2024-02-15 23:14:07 -0300

    Added media root path to uploads of the images

--- django_core/settings.py
+++ django_core/settings.py
@@ -117,9 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
 # Added by me
 MEDIA_URL = 'media/'
-STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = Path(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
