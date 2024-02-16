c2f674a5850a221040b8f9c43b779909bc3db6d4
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2024-02-15 23:06:49 -0300

    Added the magic __str__ function to the Category and Recipe model to return a more intuitive representation of the object names

--- recipes/models.py
+++ recipes/models.py
@@ -6,6 +6,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -23,3 +26,6 @@ class Recipe(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
