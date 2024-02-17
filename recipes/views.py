ea811efea00c5465ebaf9960b2d33b4a5cbec036
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2024-02-16 21:31:06 -0300

    Replaced mocked data with real data for the recipe model

--- recipes/views.py
+++ recipes/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.shortcuts import render
 
 from utils.recipes.factory import make_recipe
+from recipes.models import Recipe
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     context = {
         'title': 'Home',
-        'recipes': [make_recipe() for _ in range(6)],
+        'recipes': recipes,
     }
     return render(request, 'pages/home.html', context)
 
